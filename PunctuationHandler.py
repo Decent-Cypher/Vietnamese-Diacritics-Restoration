5a8a67884c56e4c0bea7ff090bfb119173dd2933
AndrewNguyen4 <153667379+AndrewNguyen4@users.noreply.github.com>
2024-05-26 19:51:00 +0700

    Update PunctuationHandler.py
    - Two modes: Toggle capital_only_mode to activate
    - Now can handle single words containing punctuation only.
    Warning: Does not work when there are multiple consecutive words of punctuation only, e.g. "Word ?? abc" works, "Word ? ? abc" doesn't.

--- PunctuationHandler.py
+++ PunctuationHandler.py
@@ -1,9 +1,13 @@
+import re
+
 class PunctuationHandler():
+    capital_only_mode = False
     special_characters = "!@#$%^&*()-+?_=,<>'/\""
     # current_input = []     input split by spaces. E.g. This is ("A sentence").       ->      ["This", "is", "(\"a", "sentence\")."]
     pre_punctuation = []   # List of special characters at the beginning of each word  ->      ['', '', '("', '']
     post_punctuation = []  # List of special characters at the end of each word        ->      ['', '', '', '").']
     capitalization = []    # List of indices of capitalized words.                     ->      [0,2]
+    punctuated_words = dict()
     
     def __init__(self) -> None:
         pass
@@ -12,6 +16,13 @@ class PunctuationHandler():
         current_input = input.split()
         for i in range(len(current_input)):
             current_word = current_input[i]
+            # Check if the word is full of special characters
+            if re.search("[A-Za-z0-9]",current_word) == None:
+                self.punctuated_words[i] = current_word
+                current_input[i] = ''
+                # self.pre_punctuation.append('')
+                # self.post_punctuation.append('')
+                continue
             
             # Handling pre-punctuation
             if len(current_word) > 1 and current_word[1] in self.special_characters:
@@ -23,7 +34,7 @@ class PunctuationHandler():
                 current_word = current_word[1:]
                 current_input[i] = current_word
             else:
-                self.pre_punctuation.append("")
+                self.pre_punctuation.append('')
             
             # Handling post-punctuation
             if len(current_word) > 3 and current_word[-3] in self.special_characters:
@@ -39,20 +50,28 @@ class PunctuationHandler():
                 current_word = current_word[:-1]
                 current_input[i] = current_word
             else:
-                self.post_punctuation.append("")       
+                self.post_punctuation.append('')       
                 
             # Handling capitalization
             if current_word[0].isupper():
                 self.capitalization.append(i)
                 current_input[i] = current_word.lower()
+        
+        while '' in current_input:
+            current_input.remove('')
             
-        output = ' '.join(current_input)
-        return output
+        if not self.capital_only_mode:
+            output = ' '.join(current_input)
+            return output
+        else:
+            return input.lower()
             
     def converter(self, input):
+        input = ''.join(e for e in input if e.isalnum() or e == ' ')
         current_input = input.split()
         for i in range(len(current_input)):
-            current_word = current_input[i]
+            if i in self.punctuated_words:
+                current_input[i-1] += ' ' + self.punctuated_words[i]
             
             # Return capitalization
             if i in self.capitalization:
@@ -68,8 +87,13 @@ class PunctuationHandler():
         return output
 
 if __name__ == '__main__':
-    sentence = 'Day la ("mot cau").'
-    sentence2 = 'đây là một câu'
+    sentence = 'Day la . ("mot cau").'
+    sentence2 = 'đây là . ("một câu").'
+    sentence3 = 'đây là một câu'
     a = PunctuationHandler()
+    a.capital_only_mode = True
     print(a.remover(sentence))
     print(a.converter(sentence2))
+    a.capital_only_mode = False
+    print(a.remover(sentence))
+    print(a.converter(sentence3))
